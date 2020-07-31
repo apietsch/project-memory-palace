270bd52a9f00383eb802b8e72adb5f53cc7cd7ed
Alexander Pietsch <alexander.pietsch@pollmeier.com>
2020-07-31 16:09:17 +0200

    initial project from vs, switched to fixed ssl port

---
+++ MemoryPalaceApp/MemoryPalaceApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MemoryPalaceApp.Models;
+
+namespace MemoryPalaceApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
